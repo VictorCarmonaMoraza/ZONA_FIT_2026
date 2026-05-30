5f7b96f265693be4ef87151fbf2244b6096065ca
Victor Carmona <VictorCarmona9035@hotmail.com>
2026-05-30 20:15:16 +0200

    Creacion Clase ClienteServicio con Spring

--- src/main/java/gm/zona_fit/servicio/IClienteServicio.java
+++ src/main/java/gm/zona_fit/servicio/IClienteServicio.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface IClienteServicio {
     public List<Cliente> listarClientes();
-    public Cliente buscarClientePorId(Cliente cliente);
+    public Cliente buscarClientePorId(Integer idCliente);
     public void guardarCliente(Cliente cliente);
     public void eliminarCliente(Cliente cliente);
 }
